f0bebf1181fa3206524024de02ba2b8efc4783f6
Dedios <dediosprogramming@gmail.com>
2022-09-29 17:36:12 -0400

    refactor

--- script.js
+++ script.js
@@ -2,57 +2,57 @@
 
 const billInput = document.getElementById('billTotalInput')
 const tipInput = document.getElementById('tipInput')
-const numbrOfPeople = document.getElementById('numberOfPeople')
+const people = document.getElementById('numberOfPeople')
 //the original code above was const numberOfPeople = document.getElementById('numberOfPeople'). 
-// Obvious problem because you cannot re-declare the scoped variable. 
-const perPersonTotalDiv = document.getElementById('perPersonTotal')
+// Obvious problem because you cannot re-declare the scoped variable. I took off the e originally but 
+//still too close to the variable name of the element I am trying to get so just changed to people 
+const perPersonTotal = document.getElementById('perPersonTotal')
 
 // gets the number of people from the number of people div
-let numberOfPeople = Number(numberOfPeopleDiv.innerHTML);
-//same issue in the line above as described on line 4. Cannot redeclare the scoped variable.
+let numberOfPeople = Number(people.innerText);
 
 
+//below is the code that will run when you enter inputs according to the tutorial
+calculateBill = () => {
+    const bill = (billInput.value)
+    const tipPercent = (billInput.value) / 100;
+    const tipAmount = bill * tipPercent;
+    const total = tipAmount + bill;
+    const perPersonTotal = total / numberOfPeople;
 
-// increases amt of people on the people component
-const increasePeople = () => {
-    numbrOfPeople += 1;
+    bill * tipPercent;
 
-    numberOfPeopleDiv.innerHTML = numbrOfPeople;
+    tipAmount + bill;
 
-    caclulateBill()
+    total / perPersonTotal;
+
+    perPersonTotal.innerText = `${perPersonTotal.toFixed(2)}`;
+
 
 }
 
 
-// decreases amt of people on the people component
+// increases amt of people on the people component
+const increasePeople = () => {
 
+    people.value += 1;
+
+    caclulateBill()
+}
+
+
+// decreases amt of people on the people component
 const decreasePeople = () => {
-    if (numbrOfPeople <= 1) {
+
+    if (people <= 1) {
         return
     } else {
-        numbrOfPeople -= 1
 
-        numberOfPeopleDiv.innerHTML = numbrOfPeople;
+        people.value -= 1
+
+        people.innerText = numbrOfPeople;
 
         caclulateBill()
     }
 
 }
-
-//below is the code that will run when you enter inputs
-caclulateBill = () => {
-    const bill = Number(billInput.value)
-
-
-    const tipPercent = Number(billInput.value) / 100;
-
-    const tipAmount = bill * tipPercent
-
-    const total = tipAmount + bill;
-
-    const perPersonTotal = total / numbrOfPeople;
-
-    persPersonTotalDiv.innerHTML = `${perPersonTotal.toFixed(2)}`
-
-}
-
