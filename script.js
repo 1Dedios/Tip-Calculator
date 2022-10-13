ec25dcf3499c4535ddea18ca96e5509167169f52
Dedios <dediosprogramming@gmail.com>
2022-10-12 20:47:59 -0400

    refactor

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-//below I am declaring the variables in order to acquire the value in the functions below
-
 const billInput = document.getElementById('billTotalInput')
 const tipInput = document.getElementById('tipInput')
-const people = document.getElementById('numberOfPeople')
-//the original code above was const numberOfPeople = document.getElementById('numberOfPeople'). 
-// Obvious problem because you cannot re-declare the scoped variable. I took off the e originally but 
-//still too close to the variable name of the element I am trying to get so just changed to people 
+const amtOfPeople = document.getElementById('numberOfPeople')
+
 const perPersonTotal = document.getElementById('perPersonTotal')
 
-// gets the number of people from the number of people div
+
 let numberOfPeople = Number(people.innerText);
 
 
-//below is the code that will run when you enter inputs according to the tutorial
+
 calculateBill = () => {
     const bill = (billInput.value)
     const tipPercent = (billInput.value) / 100;
@@ -32,27 +28,24 @@ calculateBill = () => {
 }
 
 
-// increases amt of people on the people component
+
 const increasePeople = () => {
 
-    people.value += 1;
+    amtOfPeople += 1;
 
     caclulateBill()
 }
 
 
-// decreases amt of people on the people component
+
 const decreasePeople = () => {
 
-    if (people <= 1) {
+    if (numberOfPeople <= 1) {
         return
     } else {
-
-        people.value -= 1
-
-        people.innerText = numbrOfPeople;
-
+        amtOfPeople -= 1
         caclulateBill()
+        amtOfPeople.innerText = numberOfPeople;
     }
 
 }
